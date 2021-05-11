33573eefbf5db1dd58704cc5cb277fcb14cb88c1
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2021-05-11 19:58:21 +0300

    Create a base of the payment-react-web app

---
+++ src/payment-react-web/src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { HeaderPure } from './Header';
+import './App.scss';
+
+export const App = () => {
+  return <React.Fragment>
+    <HeaderPure />
+  </React.Fragment>;
+};
