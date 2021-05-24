ee47b3873246dec50cc0af974cf458c913cab442
Maxim Kucherov <maxima-net@bk.ru>
2021-05-24 22:44:36 +0300

    Fix tab size

--- src/dashboard-react-web/src/components/common/Button/Button.tsx
+++ src/dashboard-react-web/src/components/common/Button/Button.tsx
@@ -4,33 +4,33 @@ import { Link, NavLink } from 'react-router-dom';
 import { combineClassNames } from '@tezos-payments/common/dist/utils';
 
 import {
-    ButtonProps, isLinkButtonProps, isNavLinkButtonProps,
-    LinkButtonProps, NativeButtonProps, NavLinkButtonProps
+  ButtonProps, isLinkButtonProps, isNavLinkButtonProps,
+  LinkButtonProps, NativeButtonProps, NavLinkButtonProps
 } from './ButtonProps';
 import './Button.scss';
 
 const PrimaryButton = (props: NativeButtonProps) => {
-    const className = combineClassNames('button', 'button_primary', props.className);
+  const className = combineClassNames('button', 'button_primary', props.className);
 
-    return <button {...props} type={props.htmlType} className={className}>{props.children}</button>;
+  return <button {...props} type={props.htmlType} className={className}>{props.children}</button>;
 };
 
 const LinkButton = (props: LinkButtonProps) => {
-    const className = combineClassNames('button', 'button_link', props.className);
+  const className = combineClassNames('button', 'button_link', props.className);
 
-    return <Link {...props} type={undefined} className={className}>{props.children}</Link>;
+  return <Link {...props} type={undefined} className={className}>{props.children}</Link>;
 };
 
 const NavLinkButton = (props: NavLinkButtonProps) => {
-    const className = combineClassNames('button', 'button_link', props.className);
+  const className = combineClassNames('button', 'button_link', props.className);
 
-    return <NavLink {...props} type={undefined} className={className} activeClassName={'button_link_active'}>{props.children}</NavLink>;
+  return <NavLink {...props} type={undefined} className={className} activeClassName={'button_link_active'}>{props.children}</NavLink>;
 };
 
 export const Button = (props: ButtonProps) => isLinkButtonProps(props)
-    ? <LinkButton {...props} />
-    : isNavLinkButtonProps(props)
-        ? <NavLinkButton {...props} />
-        : <PrimaryButton {...props} />;
+  ? <LinkButton {...props} />
+  : isNavLinkButtonProps(props)
+    ? <NavLinkButton {...props} />
+    : <PrimaryButton {...props} />;
 
 export const ButtonPure = React.memo(Button);
--- src/dashboard-react-web/src/components/common/Button/ButtonProps.ts
+++ src/dashboard-react-web/src/components/common/Button/ButtonProps.ts
@@ -3,31 +3,31 @@ import { NavLinkProps } from 'react-router-dom';
 export type ButtonType = 'default' | 'link' | 'nav-link';
 
 export interface ButtonPropBase {
-    type?: ButtonType;
-    className?: string;
-    children?: React.ReactNode;
+  type?: ButtonType;
+  className?: string;
+  children?: React.ReactNode;
 }
 
 export type NativeButtonProps = ButtonPropBase & Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'type'> & {
-    type?: 'default';
-    htmlType?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
-    onClick?: React.MouseEventHandler<HTMLElement>;
+  type?: 'default';
+  htmlType?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
+  onClick?: React.MouseEventHandler<HTMLElement>;
 };
 
 export type LinkButtonProps = ButtonPropBase & Omit<NavLinkProps, 'type' | 'href'> & {
-    type: 'link';
+  type: 'link';
 };
 
 export type NavLinkButtonProps = ButtonPropBase & Omit<NavLinkProps, 'type' | 'href'> & {
-    type: 'nav-link';
+  type: 'nav-link';
 };
 
 export type ButtonProps = LinkButtonProps | NativeButtonProps | NavLinkButtonProps;
 
 export const isLinkButtonProps = (props: ButtonProps): props is LinkButtonProps => {
-    return props.type === 'link';
+  return props.type === 'link';
 };
 
 export const isNavLinkButtonProps = (props: ButtonProps): props is NavLinkButtonProps => {
-    return props.type === 'nav-link';
+  return props.type === 'nav-link';
 };
