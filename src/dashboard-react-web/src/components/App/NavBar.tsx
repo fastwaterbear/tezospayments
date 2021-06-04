5b16dcc5e895b05d13e5763a809e903e92ee9ec3
Maxim Kucherov <maxima-net@bk.ru>
2021-06-04 23:14:44 +0300

    Use react-router in navbar

--- src/dashboard-react-web/src/components/App/NavBar.tsx
+++ src/dashboard-react-web/src/components/App/NavBar.tsx
@@ -1,17 +1,20 @@
 import { UserOutlined, DashboardOutlined, UnorderedListOutlined, ProfileOutlined, InfoCircleOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
+import { config } from '../../config';
 import { useCurrentLanguageResources } from '../hooks';
 import './NavBar.scss';
 
 const NavBar = () => {
+  const location = useLocation();
   const langResources = useCurrentLanguageResources();
   const viewsLangResources = langResources.views;
 
   return <nav>
-    <Menu className="navbar" mode="inline">
-      <Menu.Item key="1" icon={<DashboardOutlined />}>{viewsLangResources.overview.title}</Menu.Item>
+    <Menu className="navbar" mode="inline" selectedKeys={[location.pathname]}>
+      <Menu.Item key={config.routers.overview} icon={<DashboardOutlined />}><Link to={config.routers.overview}>{viewsLangResources.overview.title}</Link></Menu.Item>
       <Menu.Item key="2" icon={<UnorderedListOutlined />}>{viewsLangResources.operations.title}</Menu.Item>
       <Menu.Item key="3" icon={<UserOutlined />}>{viewsLangResources.services.title}</Menu.Item>
       <Menu.Item key="4" icon={<ProfileOutlined />}>{viewsLangResources.settings.title}</Menu.Item>
