832eb0015a4bd3c45338e037a9ac2c30e5267845
Maxim Kucherov <maxima-net@bk.ru>
2021-06-12 22:41:40 +0300

    Make navbar sticky

--- src/dashboard-react-web/src/components/App/NavBar.tsx
+++ src/dashboard-react-web/src/components/App/NavBar.tsx
@@ -1,5 +1,5 @@
 import { SettingOutlined, DashboardOutlined, UnorderedListOutlined, ProfileOutlined, InfoCircleOutlined } from '@ant-design/icons';
-import { Menu, Button } from 'antd';
+import { Menu, Button, Affix } from 'antd';
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
@@ -12,22 +12,24 @@ const NavBar = () => {
   const langResources = useCurrentLanguageResources();
   const viewsLangResources = langResources.views;
 
-  return <nav className="navbar">
-    <Menu className="navbar__menu" mode="inline" selectedKeys={[location.pathname]}>
-      <Menu.Item key={config.routers.overview} icon={<DashboardOutlined />}>
-        <Link to={config.routers.overview}>{viewsLangResources.overview.title}</Link>
-      </Menu.Item>
-      <Menu.Item key="2" icon={<UnorderedListOutlined />}>{viewsLangResources.operations.title}</Menu.Item>
-      <Menu.Item key="3" icon={<ProfileOutlined />}>{viewsLangResources.services.title}</Menu.Item>
-      <Menu.Item key="4" icon={<SettingOutlined />}>{viewsLangResources.settings.title}</Menu.Item >
-      <Menu.Item key="5" icon={<InfoCircleOutlined />}>{viewsLangResources.about.title}</Menu.Item>
-    </Menu>
+  return <Affix className="navbar-container">
+    <nav className="navbar">
+      <Menu className="navbar__menu" mode="inline" selectedKeys={[location.pathname]}>
+        <Menu.Item key={config.routers.overview} icon={<DashboardOutlined />}>
+          <Link to={config.routers.overview}>{viewsLangResources.overview.title}</Link>
+        </Menu.Item>
+        <Menu.Item key="2" icon={<UnorderedListOutlined />}>{viewsLangResources.operations.title}</Menu.Item>
+        <Menu.Item key="3" icon={<ProfileOutlined />}>{viewsLangResources.services.title}</Menu.Item>
+        <Menu.Item key="4" icon={<SettingOutlined />}>{viewsLangResources.settings.title}</Menu.Item >
+        <Menu.Item key="5" icon={<InfoCircleOutlined />}>{viewsLangResources.about.title}</Menu.Item>
+      </Menu>
 
-    <div className="navbar__bottom-buttons">
-      <Button>{viewsLangResources.createService.title}</Button>
-      <Button type="primary">{viewsLangResources.acceptPayments.title}</Button>
-    </div>
-  </nav>;
+      <div className="navbar__bottom-buttons">
+        <Button>{viewsLangResources.createService.title}</Button>
+        <Button type="primary">{viewsLangResources.acceptPayments.title}</Button>
+      </div>
+    </nav>
+  </Affix >;
 };
 
 export const NavBarPure = React.memo(NavBar);
