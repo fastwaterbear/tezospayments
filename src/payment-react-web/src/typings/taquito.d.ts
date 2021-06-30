4e4aa5a163f43d497f41bae9c13e93f8ee4f5ca7
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2021-07-01 01:18:39 +0300

    Extract the send parameters of taquito contract method; Allow to use big numbers for the amount parameter;

---
+++ src/payment-react-web/src/typings/taquito.d.ts
@@ -0,0 +1,17 @@
+import { ContractProvider, Wallet, TransactionWalletOperation, TransactionOperation } from '@taquito/taquito';
+import { BigNumber } from 'bignumber.js';
+
+interface SendParams {
+  fee?: number;
+  storageLimit?: number;
+  gasLimit?: number;
+  amount: number | BigNumber;
+  source?: string;
+  mutez?: boolean;
+}
+
+declare module '@taquito/taquito' {
+  interface ContractMethod<T extends ContractProvider | Wallet> {
+    send(params?: SendParameters): Promise<T extends Wallet ? TransactionWalletOperation : TransactionOperation>;
+  }
+}
