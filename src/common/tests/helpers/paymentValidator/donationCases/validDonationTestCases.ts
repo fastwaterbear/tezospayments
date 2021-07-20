9f53f2e58ddd9679778e75ba842220097b7e8fd1
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2021-07-21 01:20:06 +0300

    Fix test cases, remove excess properties

--- src/common/tests/helpers/paymentValidator/donationCases/validDonationTestCases.ts
+++ src/common/tests/helpers/paymentValidator/donationCases/validDonationTestCases.ts
@@ -7,23 +7,20 @@ export default [
   {
     type: PaymentType.Donation,
     targetAddress: 'KT1J5rXFQMG2iHfA4EhpKdFyQVQAVY8wHf6x',
-    amount: new BigNumber(384.48302),
-    created: new Date('2021-07-04T00:17:23.043Z'),
+    desiredAmount: new BigNumber(384.48302),
     urls: []
   },
   {
     type: PaymentType.Donation,
     targetAddress: 'KT1J5rXFQMG2iHfA4EhpKdFyQVQAVY8wHf6x',
-    amount: new BigNumber(20238382),
-    asset: 'KT1Mn2HUUKUPg8wiQhUJ8Z9jUtZLaZn8EWL2',
-    created: new Date('2021-07-05T10:21:01.111Z'),
+    desiredAmount: new BigNumber(20238382),
+    desiredAsset: 'KT1Mn2HUUKUPg8wiQhUJ8Z9jUtZLaZn8EWL2',
     urls: []
   },
   {
     type: PaymentType.Donation,
     targetAddress: 'KT1J5rXFQMG2iHfA4EhpKdFyQVQAVY8wHf6x',
     amount: new BigNumber(67.49),
-    created: new Date('2021-07-05T10:21:01.111Z'),
     successUrl: new URL('https://fastwaterbear.com/tezosdonations/test/donation/success'),
     cancelUrl: new URL('https://fastwaterbear.com/tezosdonations/test/donation/cancel'),
     urls: []
@@ -31,9 +28,8 @@ export default [
   {
     type: PaymentType.Donation,
     targetAddress: 'KT1J5rXFQMG2iHfA4EhpKdFyQVQAVY8wHf6x',
-    amount: new BigNumber(23267.41173),
+    desiredAmount: new BigNumber(23267.41173),
     successUrl: new URL('https://fastwaterbear.com/tezosdonations/test/donation/success'),
-    created: new Date('2021-07-05T10:21:01.111Z'),
     urls: []
   },
 ] as readonly Donation[];
