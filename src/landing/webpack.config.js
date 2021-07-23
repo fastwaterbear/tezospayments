f61c2d7ac262b291799066cc67f06823d468d09c
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2021-07-23 18:39:29 +0300

    Add css loaders to the landing webpack; Create the custom InlineChunkHtml webpack plugin

--- src/landing/webpack.config.js
+++ src/landing/webpack.config.js
@@ -1,17 +1,49 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const InlineChunkHtmlPlugin = require('./webpack/inline-chunk-html-plugin');
 
 module.exports = {
   mode: 'production',
-  entry: {},
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
+      inject: true,
       template: './src/index.html',
-    })
+      minify: {
+        collapseWhitespace: true,
+        keepClosingSlash: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      }
+    }),
+    new InlineChunkHtmlPlugin(HtmlWebpackPlugin, []),
+    new MiniCssExtractPlugin()
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash:8].js',
     clean: true
-  }
+  },
+  optimization: {
+    minimizer: [
+      '...',
+      new CssMinimizerPlugin(),
+    ],
+  },
 }
---
+++ src/landing/webpack/inline-chunk-html-plugin.js
@@ -0,0 +1,60 @@
+'use strict';
+
+class InlineChunkHtmlPlugin {
+  static inlinedTagInfos = new Map()
+    .set('script', {
+      sourceTagName: 'script',
+      targetTagName: 'script',
+      getAssetName: tag => tag && tag.attributes && tag.attributes.src,
+    })
+    .set('link', {
+      sourceTagName: 'link',
+      targetTagName: 'style',
+      getAssetName: tag => tag && tag.attributes && tag.attributes.href,
+    });
+
+  constructor(htmlWebpackPlugin) {
+    this.htmlWebpackPlugin = htmlWebpackPlugin;
+  }
+
+  apply(compiler) {
+    compiler.hooks.compilation.tap('InlineChunkHtmlPlugin', compilation => {
+      const deletedAssets = new Set();
+      const tagFunction = tag => {
+        const inlinedTag = this.getInlinedTag(compilation.assets, tag);
+        if (inlinedTag !== tag && inlinedTag.assetName)
+          deletedAssets.add(inlinedTag.assetName);
+
+        return inlinedTag;
+      }
+      const hooks = this.htmlWebpackPlugin.getHooks(compilation);
+
+      hooks.alterAssetTagGroups.tap('InlineChunkHtmlPlugin', assets => {
+        assets.headTags = assets.headTags.map(tagFunction);
+        assets.bodyTags = assets.bodyTags.map(tagFunction);
+      });
+
+      hooks.afterEmit.tap('InlineChunkHtmlPlugin', () => {
+        for (const assetName of deletedAssets)
+          delete compilation.assets[assetName];
+      });
+    });
+  }
+
+  getInlinedTag(assets, tag) {
+    const tagInfo = InlineChunkHtmlPlugin.inlinedTagInfos.get(tag.tagName);
+    if (!tagInfo)
+      return tag;
+
+    const assetName = tagInfo.getAssetName(tag);
+    if (!assetName)
+      return tag;
+
+    const asset = assets[assetName];
+    return asset
+      ? { tagName: tagInfo.targetTagName, innerHTML: asset.source(), closeTag: true, assetName }
+      : tag
+  }
+}
+
+module.exports = InlineChunkHtmlPlugin;
