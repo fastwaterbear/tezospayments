76db276ca471ed7eb651bc15c72d1b4d6bfe9802
Maxim Kucherov <maxima-net@bk.ru>
2021-08-06 21:16:35 +0300

    Fix icon

--- src/dashboard-react-web/src/components/common/Tags/ActiveTag/ActiveTag.tsx
+++ src/dashboard-react-web/src/components/common/Tags/ActiveTag/ActiveTag.tsx
@@ -1,4 +1,4 @@
-import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
+import { CheckCircleOutlined, CloseCircleOutlined, PauseCircleOutlined } from '@ant-design/icons';
 import { Tag } from 'antd';
 import React from 'react';
 
@@ -18,7 +18,7 @@ export const ActiveTag = (props: ActiveTagProps) => {
   return props.deleted
     ? <Tag icon={<CloseCircleOutlined />} className="tag-active tag-active_deleted">{servicesLangResources.status.deleted}</Tag>
     : props.paused
-      ? <Tag icon={<CloseCircleOutlined />} className="tag-active tag-active_paused">{servicesLangResources.status.paused}</Tag>
+      ? <Tag icon={<PauseCircleOutlined />} className="tag-active tag-active_paused">{servicesLangResources.status.paused}</Tag>
       : <Tag icon={<CheckCircleOutlined />} className="tag-active tag-active_active">{servicesLangResources.status.active}</Tag>;
 };
 
