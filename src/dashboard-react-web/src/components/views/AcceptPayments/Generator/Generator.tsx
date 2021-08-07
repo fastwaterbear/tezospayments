47d2f31be889ebde737c5ee0f984a600670b2461
Maxim Kucherov <maxima-net@bk.ru>
2021-08-07 21:02:14 +0300

    Implement Generator Pane UI

--- src/dashboard-react-web/src/components/views/AcceptPayments/Generator/Generator.tsx
+++ src/dashboard-react-web/src/components/views/AcceptPayments/Generator/Generator.tsx
@@ -1,7 +1,36 @@
+import { Card } from 'antd';
 import React from 'react';
 
+import { ExternalLink } from '../../../common';
+import { useCurrentLanguageResources } from '../../../hooks';
+
+import './Generator.scss';
+
 export const Generator = () => {
-  return <div>Generator</div>;
+  const langResources = useCurrentLanguageResources();
+  const acceptPaymentsLangResources = langResources.views.acceptPayments;
+
+  const tabList = [
+    { key: 'directLink', tab: 'Direct Link' },
+    { key: 'widget', tab: 'Widget', disabled: true },
+    { key: 'typescript', tab: 'TypeScript Library', disabled: true },
+    { key: 'dotnet', tab: '.NET Library', disabled: true },
+  ];
+
+  // eslint-disable-next-line max-len
+  const url = 'https://payment.tezospayments.com/KT1TwUi5inZPTJhGBhx1mvCEhWHLYpniKmTB/payment#eyJhbW91bnQiOiIxOS45OSIsImRhdGEiOnsicHVibGljIjp7Im9yZGVySWQiOiJmNTAwMDg0Mi1jNGY5LTRhM2UtYTJiZS0xMThkNTUxMjQ1ZjcifX0sImNyZWF0ZWQiOjE2Mjc4OTM3MzM0NDZ9';
+
+  return <Card
+    className="generator"
+    style={{ width: '100%' }}
+    tabList={tabList}
+    activeTabKey={tabList[0]?.key}
+  >
+    <div className="generator__direct-link">
+      <span className="generator__direct-link-help-text">{acceptPaymentsLangResources.directLinkHelpText}</span>
+      <ExternalLink className="generator__direct-link-link" href={url}>{url}</ExternalLink>
+    </div>
+  </Card >;
 };
 
 export const GeneratorPure = React.memo(Generator);
--- src/dashboard-react-web/src/localization/en-US/views/acceptPayments.ts
+++ src/dashboard-react-web/src/localization/en-US/views/acceptPayments.ts
@@ -8,4 +8,5 @@ export const acceptPayments = {
   donationData: 'Donation Data',
   shortDescription: 'Short description on the page',
   shortDescriptionHelpText: 'This message will be shown to your donators when they will donate',
+  directLinkHelpText: 'Copy the following link and paste it into your website or send to your customer directly using messengers, email, social networks, etc.'
 };
