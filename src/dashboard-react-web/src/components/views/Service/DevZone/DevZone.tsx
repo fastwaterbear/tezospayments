ee58e319e359876ee81561fa65a612fa781b8873
Maxim Kucherov <maxima-net@bk.ru>
2021-08-20 23:58:28 +0300

    Add no-data state

--- src/dashboard-react-web/src/components/views/Service/DevZone/DevZone.tsx
+++ src/dashboard-react-web/src/components/views/Service/DevZone/DevZone.tsx
@@ -1,14 +1,21 @@
 import { DeleteOutlined } from '@ant-design/icons';
 import { Button, List } from 'antd';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import './DevZone.scss';
 
 export const DevZone = () => {
-  const data = [
+  const defaultData = [
     { name: 'Home PC', apiKey: 'SHA256:4T5E9yJjajhHazEUPHAYMa97bg7m6SxpInoxtX+XLPg' },
     { name: 'Work', apiKey: 'SHA256:Q0TPyOEbRpCZyoYRFyeGmzb4yg/ujidrRRoRnyJiuqo' },
   ];
+
+  const [apiKeys, setApiKeys] = useState(defaultData);
+
+  const handleRemoveItem = useCallback((key: string) => {
+    setApiKeys(apiKeys.filter(i => i.apiKey !== key));
+  }, [apiKeys]);
+
   return <>
     <div className="service-danger-zone__button-container">
       <span className="service-dev-zone__header">{'API Keys'}</span>
@@ -16,20 +23,20 @@ export const DevZone = () => {
         {'Add Key'}
       </Button>
     </div>
-    <List
-      size="small"
-      bordered
-      dataSource={data}
-      renderItem={item => (
-        <List.Item className="api-keys__item">
-          <span>{item.name}</span>
+    <List size="small" bordered>
+      {apiKeys.length
+        ? apiKeys.map(i => <List.Item key={i.apiKey} className="api-keys__item">
+          <span>{i.name}</span>
           <div>
-            <span className="api-keys__item-key">{item.apiKey}</span>
-            <Button icon={<DeleteOutlined />} danger type="link"></Button>
+            <span className="api-keys__item-key">{i.apiKey}</span>
+            <Button onClick={() => handleRemoveItem(i.apiKey)} icon={<DeleteOutlined />} danger type="link"></Button>
           </div>
         </List.Item>
-      )}
-    />
+        ) :
+        <List.Item className="api-keys__item api-keys__item_empty">
+          {'No API keys added yet'}
+        </List.Item>}
+    </List>
   </>;
 };
 
