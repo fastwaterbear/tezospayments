c1de4dae4ea97bfdf3e45c5dcb481a2bc31cb722
Maxim Kucherov <maxima-net@bk.ru>
2021-08-23 23:13:00 +0300

    Add state

--- src/dashboard-react-web/src/components/views/Service/DevZone/AddApiKeyModal/AddApiKeyModal.tsx
+++ src/dashboard-react-web/src/components/views/Service/DevZone/AddApiKeyModal/AddApiKeyModal.tsx
@@ -1,7 +1,7 @@
 import { CopyOutlined } from '@ant-design/icons';
-import { Button, Divider, Input, Modal, Radio } from 'antd';
+import { Button, Divider, Input, Modal, Radio, RadioChangeEvent } from 'antd';
 import Search from 'antd/lib/input/Search';
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 
 import { useCurrentLanguageResources } from '../../../../hooks';
 
@@ -22,14 +22,24 @@ export const AddApiKeyModal = (props: AddApiKeyModalProps) => {
     { label: 'P256', value: 'P256' },
   ];
 
-  const publicKey = 'edpkvQXtVcy8YrBLmMhn8EDt4Zb46TWZX1QUxxepFzJgsWU6YKadJP';
-  const secretKey = 'edskRwse4Z8ZZNCC7xzCEUrTBtCeEPhv8gfBiWrE8cysRQpz45HCQjChdDckNEBZZMCxjPMkHhmGkUnwBs22cKr2nrwiGfQHsP';
+  const [algorithType, setAlgorithType] = useState(algorithmOptions[0]?.value || '');
+  const handleAlgorithTypeChanges = useCallback((e: RadioChangeEvent) => {
+    setAlgorithType(e.target.value);
+  }, []);
+
+  const [name, setName] = useState('');
+  const handleNameChanged = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  }, []);
+
+  const [publicKey, secretKey] = getKeys(algorithType);
 
   const publicKeyRef = useRef<Input>(null);
   const secretKeyRef = useRef<Input>(null);
 
   const handleCopyClick = useCallback((ref: React.RefObject<Input>) => {
     const input = ref.current;
+
     if (input) {
       input.select();
       navigator.clipboard.writeText(input.state.value);
@@ -37,11 +47,11 @@ export const AddApiKeyModal = (props: AddApiKeyModalProps) => {
   }, []);
 
   return <Modal className="api-key-modal" title={servicesLangResources.devZone.addKey} centered destroyOnClose visible={props.visible}
-    onCancel={props.onCancel} okText={servicesLangResources.devZone.saveKeys} >
+    onCancel={props.onCancel} okText={servicesLangResources.devZone.saveKeys} okButtonProps={{ disabled: !name }}>
     <span className="api-key-modal__label">{servicesLangResources.devZone.name}:</span>
-    <Input autoFocus />
+    <Input autoFocus value={name} onChange={handleNameChanged} />
     <span className="api-key-modal__label">{servicesLangResources.devZone.algorithm}:</span>
-    <Radio.Group options={algorithmOptions} value={algorithmOptions[0]?.value} />
+    <Radio.Group options={algorithmOptions} value={algorithType} onChange={handleAlgorithTypeChanges} />
     <Divider />
     <span className="api-key-modal__label">{servicesLangResources.devZone.publicKey}:</span>
     <Search ref={publicKeyRef} readOnly value={publicKey} enterButton={<Button icon={<CopyOutlined />} />} onSearch={() => handleCopyClick(publicKeyRef)} />
@@ -52,3 +62,17 @@ export const AddApiKeyModal = (props: AddApiKeyModalProps) => {
 };
 
 export const AddApiKeyModalPure = React.memo(AddApiKeyModal);
+
+const getKeys = (type: string) => {
+  switch (type) {
+    case 'Ed25519':
+      return ['edpkvQXtVcy8YrBLmMhn8EDt4Zb46TWZX1QUxxepFzJgsWU6YKadJP', 'edskRwse4Z8ZZNCC7xzCEUrTBtCeEPhv8gfBiWrE8cysRQpz45HCQjChdDckNEBZZMCxjPMkHhmGkUnwBs22cKr2nrwiGfQHsP'];
+    case 'Secp256k1':
+      return ['sppk7cjayJkatAA6Kzd9w6DSRrRDq6JoRAfugi1fqahpyjCBRCLGfob', 'spsk1VyxSVYfX3CfNpSNDxBdR97LnAdQA59jWpc4HYXtYg1cX53V6Y'];
+    case 'P256':
+      return ['p2pk65H621C6fgfcuhiUcQmx3GRz8iNxjzMB1BG2WQKAYKEcbVzyJMD', 'p2sk2yHAwhsLaRCm8zAfN4K1Py7fBhxgR1kAB5SCYn9yGWofkjqNTN'];
+
+    default:
+      throw new Error('Unknown algorithm type');
+  }
+};
