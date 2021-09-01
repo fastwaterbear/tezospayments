ef35363afae187b5f5a31e35335d1db976311e34
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2021-09-01 22:28:45 +0300

    Export internal types as the internal module

--- src/tezospayments/src/index.ts
+++ src/tezospayments/src/index.ts
@@ -1,5 +1,17 @@
+import constants from './constants';
+import * as errors from './errors';
+import * as paymentUrlFactories from './paymentUrlFactories';
+import * as signers from './signers';
+
 export type { Payment } from './models';
 export { SigningType, PaymentUrlType } from './models';
 
 export type { TezosPaymentsOptions, PaymentCreateParameters } from './options';
 export { TezosPayments } from './tezosPayments';
+
+export const internal = {
+  constants,
+  errors,
+  paymentUrlFactories,
+  signers
+};
