f5092eb0a4a1e69c2abaf918c241487e5af756a7
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2021-09-03 11:24:01 +0300

    Add the id field to the PaymentCreateParameters

--- src/tezospayments/tests/testCases/createPaymentTestCases/index.ts
+++ src/tezospayments/tests/testCases/createPaymentTestCases/index.ts
@@ -2,3 +2,11 @@ export { default as validPaymentTestCases } from './validPaymentTestCases';
 export { default as invalidPaymentTestCases } from './invalidPaymentTestCases';
 
 export { default as invalidAmountTestCases } from './invalidAmountTestCases';
+export { default as invalidAssetTestCases } from './invalidAssetTestCases';
+export { default as invalidCancelUrlTestCases } from './invalidCancelUrlTestCases';
+export { default as invalidCreatedDateTestCases } from './invalidCreatedDateTestCases';
+export { default as invalidTezosPaymentsOptionsTestCases } from './invalidDefaultPaymentParametersTestCases';
+export { default as invalidExpiredDateTestCases } from './invalidExpiredDateTestCases';
+export { default as invalidIdTestCases } from './invalidIdTestCases';
+export { default as invalidPaymentParametersObjectTestCases } from './invalidPaymentParametersObjectTestCases';
+export { default as invalidSuccessUrlTestCases } from './invalidSuccessUrlTestCases';
