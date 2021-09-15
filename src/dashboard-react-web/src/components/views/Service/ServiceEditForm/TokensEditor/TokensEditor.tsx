9a08e494367317f4c9cf30b112d85a2b24611220
Maxim Kucherov <maxima-net@bk.ru>
2021-09-15 22:22:15 +0300

    Refactor; Display image

--- src/dashboard-react-web/src/components/views/Service/ServiceEditForm/TokensEditor/TokensEditor.tsx
+++ src/dashboard-react-web/src/components/views/Service/ServiceEditForm/TokensEditor/TokensEditor.tsx
@@ -65,20 +65,21 @@ export const TokensEditor = ({ service }: TokensEditorProps) => {
   const unSelectedItems = items.filter(i => selectedTickers.every(t => t !== i.ticker));
 
   const menu = <Menu>
-    {unSelectedItems.map(i => <Menu.Item key={i.ticker} onClick={() => handleAdd(i.ticker)}>
-      <span className="token-list-item__ticker">{i.ticker}</span>
-      <span className="token-list-item__name">{i.name}</span>
+    {unSelectedItems.map(i => <Menu.Item className="token-list-editor-menu-item" key={i.ticker} onClick={() => handleAdd(i.ticker)}>
+      <img className="token-list-editor-menu-item__icon" src={i.iconSrc} alt={i.name} />
+      <span className="token-list-editor-menu-item__ticker">{i.ticker}</span>
+      <span className="token-list-editor-menu-item__name">{i.name}</span>
     </Menu.Item >)}
   </Menu>;
 
   return <>
     <ul className="token-list-editor">
-      {selectedItems.map(i => <TokenListItem className="service-token-list-item" key={i.ticker} name={i.name}
+      {selectedItems.map(i => <TokenListItem className="token-list-editor__item" key={i.ticker} name={i.name}
         ticker={i.ticker} iconSrc={i.iconSrc} handleDelete={i.ticker !== tezosMeta.symbol ? handleDelete : undefined} />)}
     </ul>
 
     {unSelectedItems.length > 0 && <Dropdown trigger={['click']} overlay={menu} placement="bottomCenter">
-      <Button className="service-edit__button" icon={<PlusOutlined />}>
+      <Button className="token-list-editor__add-button" icon={<PlusOutlined />}>
         {servicesLangResources.editing.addCurrency}
       </Button>
     </Dropdown>}
