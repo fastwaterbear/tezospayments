11b5e77a1526a570a5184107de921cfaa5156058
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2021-09-22 01:23:34 +0300

    Fix importing

--- src/dashboard-react-web/src/components/views/Service/ActionsZone/ActionsZone.tsx
+++ src/dashboard-react-web/src/components/views/Service/ActionsZone/ActionsZone.tsx
@@ -2,7 +2,7 @@ import { Button } from 'antd';
 import React, { useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { Service } from '@tezospayments/common/src';
+import { Service } from '@tezospayments/common';
 
 import { config } from '../../../../config';
 import { useCurrentLanguageResources } from '../../../hooks';
