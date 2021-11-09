e544dcfd939f412cd1af2648a3e5b8e8a5259c8f
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2021-11-09 04:37:14 +0300

    Support custom payment data fields

--- src/payment-react-web/src/components/views/Payment/PaymentDetails/PaymentDetails.tsx
+++ src/payment-react-web/src/components/views/Payment/PaymentDetails/PaymentDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { Payment } from '@tezospayments/common';
 
@@ -11,12 +11,28 @@ interface PaymentDetailsProps {
 }
 
 export const PaymentDetails = (props: PaymentDetailsProps) => {
+  const paymentDataFields = useMemo(() => props.paymentData && Object.keys(props.paymentData)
+    .map(name => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const value: any = props.paymentData?.[name];
+      return (value !== undefined && typeof value !== 'object')
+        ? { name, value }
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        : null!;
+    })
+    .filter(Boolean),
+    [props.paymentData]
+  );
+
   return <div className="payment-details">
     {<>
       <h2 className="payment-details__title">Payment Data</h2>
       <table className="payment-details__fields">
         <tbody>
           <PaymentField fieldName="Payment ID" fieldValue={props.paymentId} copyButtonEnabled={true} />
+          {paymentDataFields && paymentDataFields.map(paymentDataField =>
+            <PaymentField fieldName={paymentDataField.name} fieldValue={paymentDataField.value.toString()} copyButtonEnabled={false} />
+          )}
         </tbody>
       </table>
     </>}
