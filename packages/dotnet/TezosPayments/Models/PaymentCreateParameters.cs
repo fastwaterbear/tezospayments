a8fb2d4215a43b11aa8134d72c53667b3026f08c
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2021-12-05 19:40:45 +0300

    Add tezos payments options

---
+++ packages/dotnet/TezosPayments/Models/PaymentCreateParameters.cs
@@ -0,0 +1,12 @@
+﻿namespace TezosPayments.Models;
+
+public record PaymentCreateParameters(string Amount)
+{
+    public string? Id { get; init; }
+    public PaymentAsset? Asset { get; init; }
+    public object? Data { get; init; }
+    public DateTime? Created { get; init; }
+    public DateTime? Expired { get; init; }
+    public string? SuccessUrl { get; init; }
+    public string? CancelUrl { get; init; }
+}
---
+++ packages/dotnet/TezosPayments/Options/TezosPaymentsOptions.cs
@@ -0,0 +1,15 @@
+﻿using TezosPayments.Models;
+
+namespace TezosPayments.Options;
+
+public record TezosPaymentsOptions(
+    string ServiceContractAddress,
+    ITezosPaymentsSigningOptions Signing,
+    DefaultPaymentParameters? DefaultPaymentParameters
+);
+
+public record DefaultPaymentParameters
+{
+    public Network? Network { get; set; }
+    public PaymentUrlType UrlType { get; set; }
+}
