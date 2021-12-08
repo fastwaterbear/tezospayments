facd57f6b890108445410bc632668aa8ee235cce
Maxim Kucherov <maxima-net@bk.ru>
2021-12-08 22:44:27 +0300

    Add test chart

--- apps/dashboard-react-web/src/components/views/Analytics/Donations/Donations.tsx
+++ apps/dashboard-react-web/src/components/views/Analytics/Donations/Donations.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export const Donations = () => {
-  return <div>Donations</div>;
+  return <div>
+    <h2>Donations</h2>
+  </div>;
 };
 
 export const DonationsPure = React.memo(Donations);
--- apps/dashboard-react-web/src/components/views/Analytics/Services/Services.tsx
+++ apps/dashboard-react-web/src/components/views/Analytics/Services/Services.tsx
@@ -1,7 +1,61 @@
 import React from 'react';
 
+import { ChartPure } from '../../../common/Chart';
+
 export const Services = () => {
-  return <div>Services</div>;
+  const option: React.ComponentProps<typeof ChartPure>['option'] = {
+    dataset: {
+      source: [
+        ['Commodity', 'Owned', 'Financed'],
+        ['Commodity 1', 4, 1],
+        ['Commodity 2', 2, 4],
+        ['Commodity 3', 3, 6],
+        ['Commodity 4', 5, 3],
+      ],
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow',
+      },
+    },
+    legend: {
+      data: ['Owned', 'Financed'],
+    },
+    grid: {
+      left: '10%',
+      right: '0%',
+      top: '20%',
+      bottom: '20%',
+    },
+    xAxis: {
+      type: 'value',
+    },
+    yAxis: {
+      type: 'category',
+    },
+    series: [
+      {
+        type: 'bar',
+        stack: 'total',
+        label: {
+          show: true,
+        },
+      },
+      {
+        type: 'bar',
+        stack: 'total',
+        label: {
+          show: true,
+        },
+      },
+    ],
+  };
+
+  return <div>
+    <h2>Services</h2>
+    <ChartPure option={option} />
+  </div>;
 };
 
 export const ServicesPure = React.memo(Services);
