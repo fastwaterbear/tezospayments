d7a93c8df0c488b9704cd0ef3c6bcf19c6740ca9
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2022-01-24 00:48:46 +0300

    Create a skeleton of the documentation stie

---
+++ apps/docs/docusaurus.config.js
@@ -0,0 +1,77 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// @ts-check
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+
+/** @type {import('@docusaurus/types').Config} */
+module.exports = {
+  title: 'Tezos Payments Documentation',
+  tagline: 'Accept payments, donations, or send invoices using all advantages of the Tezos system.',
+  url: 'https://docs.tezospayments.com',
+  baseUrl: '/',
+  organizationName: 'fastwaterbear',
+  projectName: 'tezospayments',
+  favicon: 'img/favicon.ico',
+  onBrokenLinks: 'log',
+  onBrokenMarkdownLinks: 'log',
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/fastwaterbear/tezospayments/tree/master/apps/docs',
+        },
+        pages: false,
+        blog: false,
+        gtag: false,
+        googleAnalytics: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: 'light',
+        disableSwitch: true
+      },
+      navbar: {
+        title: 'Tezos Payments',
+        items: [
+          {
+            type: 'doc',
+            docId: 'concepts/overview',
+            label: 'Concepts',
+            position: 'left',
+          },
+          {
+            type: 'doc',
+            docId: 'developers/getting-started',
+            label: 'Developers',
+            position: 'left',
+          },
+          {
+            href: 'https://github.com/fastwaterbear/tezospayments',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Tezos Payments`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
