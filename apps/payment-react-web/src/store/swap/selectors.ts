45704b6335283a532c48fc6047d501637ef1a4db
Maxim Kucherov <maxima-net@bk.ru>
2022-01-25 21:09:25 +0300

    Fix can-swap selector

--- apps/payment-react-web/src/store/swap/selectors.ts
+++ apps/payment-react-web/src/store/swap/selectors.ts
@@ -6,5 +6,5 @@ export const selectSwapState = (state: AppState) => state.swapState;
 
 export const selectCanSwap = createSelector(
   selectSwapState,
-  swap => swap && (swap.tezos || (swap.tokens && Object.keys(swap.tokens).length))
+  swap => !!swap && !!(swap.tezos || (swap.tokens && Object.keys(swap.tokens).length))
 );
