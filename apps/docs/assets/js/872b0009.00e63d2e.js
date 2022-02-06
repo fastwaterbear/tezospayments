0ee817b3012d6ee8fb8dbfa21a9cbe5364a064ab
skubarenko <skubarenko@users.noreply.github.com>
2022-02-06 20:10:06 +0000

    deploy: 94022417be66e3854b988ee7d5300f1ee20cc21f

--- apps/docs/assets/js/872b0009.00e63d2e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_tezospayments_docs=self.webpackChunk_tezospayments_docs||[]).push([[55],{7709:e=>{e.exports=JSON.parse('{"title":"Components","slug":"/category/components","permalink":"/category/components","navigation":{"previous":{"title":"Overview","permalink":"/concepts/overview"},"next":{"title":"Services","permalink":"/concepts/components/services"}}}')}}]);
